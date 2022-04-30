325566301203d8aa8fc134d9fa8c637f9750bba2
hide <hide@MacBook-Pro-user.local>
2022-04-30 20:45:29 +0300

    New repo

---
+++ test.js
@@ -0,0 +1,6 @@
+// Task 1
+// Here is JSON with data about temperature. Need to show a key with the null value. Please, use JSON
+// {"k1":36.9, "k2":37.7, "k3":null, "k4":37.3, "k5":38.1, "k6":36.3}
+let x={"k1":36.9, "k2":37.7, "k3":null, "k4":37.3, "k5":38.1, "k6":36.3}
+parseInt(x)
+console.log(x.k3, x.k1)
