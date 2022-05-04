5dde133e32ad1d29755915ce59132f79565b680b
hide <hide@MacBook-Pro-user.local>
2022-05-04 15:23:15 +0300

    Строчные функции

---
+++ Prototype.js
@@ -0,0 +1,11 @@
+// let k1 = () => console.log ("Function") //Присвоили функцию в переменную
+// k1()                       //Вызвали функцию
+//  let func1 = ( fl1 , fl2) => {console.log("Function1" , fl1, fl2)} //Обьявили функцию, которая ожидает на вход две переменные
+//  func1(3, 5)
+ let pid = 1
+ let board = 2
+ let total = (pid > 4) ? 
+ (pid1,pid2) => {console.log("Where is other?")} : 
+ (pid1,pid2) => {console.log("На косточку")}
+ total (pid)
+ 
--- test.js
+++ test.js
@@ -3,4 +3,4 @@
 // {"k1":36.9, "k2":37.7, "k3":null, "k4":37.3, "k5":38.1, "k6":36.3}
 let x={"k1":36.9, "k2":37.7, "k3":null, "k4":37.3, "k5":38.1, "k6":36.3}
 parseInt(x)
-console.log(x.k3, x.k1)
+console.log(x.k3)
